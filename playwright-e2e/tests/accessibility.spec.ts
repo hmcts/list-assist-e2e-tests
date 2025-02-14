6c77a45e4dfdfbc0964d2dfd6346866c1e6bd8dd
Jay Ashton <jay.ashton@justice.gov.uk>
2025-02-14 09:11:44 +0000

    Fix accessibility test

--- playwright-e2e/tests/accessibility.spec.ts
+++ playwright-e2e/tests/accessibility.spec.ts
@@ -5,10 +5,16 @@ test.use({
   storageState: config.users.testUser.sessionFile,
 });
 
-test("Accessibility example using custom fixture @a11y", async ({
-  homePage,
-  axeUtils,
-}) => {
-  await expect(homePage.sidebarComponent.sidebar).toBeVisible();
-  await axeUtils.audit();
+test.describe("Accessibility test example @a11y", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(config.urls.baseUrl);
+  });
+
+  test("Accessibility example using Axe fixture", async ({
+    homePage,
+    axeUtils,
+  }) => {
+    await expect(homePage.sidebarComponent.sidebar).toBeVisible();
+    await axeUtils.audit();
+  });
 });
