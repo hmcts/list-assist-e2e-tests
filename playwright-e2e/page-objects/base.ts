8548e025fc4fc5ebdbd83a2ac441c43f4b2f0c30
Rhys Heal <rhys.heal@justice.gov.uk>
2025-03-31 11:05:52 +0100

    update test assertions

--- playwright-e2e/page-objects/base.ts
+++ playwright-e2e/page-objects/base.ts
@@ -9,3 +9,12 @@ export abstract class Base {
 
   constructor(public readonly page: Page) {}
 }
+
+export function generateRandomAlphanumeric(length: number): string {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+}
