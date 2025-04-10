9bcf306365ad6dbc3fe73803e13d568694ade6b9
Rhys Heal <rhys.heal@justice.gov.uk>
2025-04-10 17:06:11 +0100

    started adding extra ui tests

--- playwright-e2e/page-objects/pages/home.po.ts
+++ playwright-e2e/page-objects/pages/home.po.ts
@@ -3,6 +3,7 @@ import { Base } from "../base";
 
 export class HomePage extends Base {
   readonly container = this.page.locator(".bodycontent");
+  readonly header = this.page.locator("h1.header-title");
 
   constructor(page: Page) {
     super(page);
