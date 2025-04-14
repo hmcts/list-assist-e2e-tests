7c586f70a0eab0be9c8ca7e663c4bda0d33770f8
Rhys Heal <rhys.heal@justice.gov.uk>
2025-04-14 15:37:22 +0100

    reduced duplication

--- playwright-e2e/tests/homepage-ui-upper-navigation.spec.ts
+++ playwright-e2e/tests/homepage-ui-upper-navigation.spec.ts
@@ -17,73 +17,22 @@ test.describe("Logout functionality @ui-test", () => {
   });
 });
 
-test.describe("Upper bar functionality @ui-test", () => {
+test.describe("Upper bar UI @ui-test", () => {
   test.use({
     storageState: config.users.testUser.sessionFile,
   });
 
-  test("Close case buttons is present and works as expected @smoke", async ({
+  test("Close case buttons is present", async ({
     homePage,
-    addNewCasePage,
-    caseSearchPage,
-    caseDetailsPage,
   }) => {
     await homePage.page.goto(config.urls.baseUrl);
     await expect(homePage.upperbarComponent.logoutButton).toBeVisible();
-
-    const hmctsCaseNumber = "HMCTS_CN_" + addNewCasePage.hmctsCaseNumber;
-    const caseName = "AUTO_" + addNewCasePage.hmctsCaseNumber;
 
     //empties cart if anything present in cart
     await homePage.sidebarComponent.emptyCaseCart();
 
     // Check if the close case button is present
     await expect(homePage.upperbarComponent.closeCaseButton).toBeVisible();
-
-    // Test data
-    const caseData = {
-      hmctsCaseNumberHeaderValue:
-        addNewCasePage.CONSTANTS.HMCTS_CASE_NUMBER_HEADER_VALUE,
-      caseNameHeaderValue: addNewCasePage.CONSTANTS.CASE_NAME_HEADER_VALUE,
-      jurisdiction: addNewCasePage.CONSTANTS.JURISDICTION_FAMILY,
-      service: addNewCasePage.CONSTANTS.SERVICE_DIVORCE,
-      caseType: addNewCasePage.CONSTANTS.DECREE_ABSOLUTE_CASE_TYPE,
-      region: addNewCasePage.CONSTANTS.REGION_WALES,
-      cluster: addNewCasePage.CONSTANTS.CLUSTER_WALES_CIVIL_FAMILY_TRIBUNALS,
-      hearingCentre: addNewCasePage.CONSTANTS.HEARING_CENTRE_CARDIFF,
-      hearingTypeRef: addNewCasePage.CONSTANTS.HEARING_TYPE_APPLICATION_REF,
-      currentStatus: addNewCasePage.CONSTANTS.CURRENT_STATUS_AWAITING_LISTING,
-    };
-
-    //create a new case
-    await homePage.sidebarComponent.openAddNewCasePage();
-    await addNewCasePage.addNewCaseWithMandatoryData(
-      caseData,
-      hmctsCaseNumber,
-      caseName,
-    );
-
-    //add case to cart
-    await caseSearchPage.sidebarComponent.openSearchCasePage();
-    await caseSearchPage.searchCase(caseName);
-
-    await caseDetailsPage.addToCartButton.click();
-    await caseDetailsPage.sidebarComponent.checkCartButtonEnabled();
-
-    //check current case drop down menu
-    await expect(
-      homePage.upperbarComponent.currentCaseDropdownButton,
-    ).toBeVisible();
-    await homePage.upperbarComponent.currentCaseDropdownButton.click();
-    await expect(
-      homePage.upperbarComponent.currentCaseDropdownList,
-    ).toContainText(homePage.upperbarComponent.currentCaseDropDownItems);
-
-    //press Close case button
-    await homePage.upperbarComponent.closeCaseButton.click();
-
-    //wait for page load
-    await homePage.waitForHomePageLoad();
   });
 
   test("Close participant button is present and works as expected @smoke", async ({
