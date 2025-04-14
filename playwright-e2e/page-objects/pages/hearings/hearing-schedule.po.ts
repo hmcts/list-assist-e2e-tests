528bd9237ef7255235f682f7f0d71a5af01ffb92
Rhys Heal <rhys.heal@justice.gov.uk>
2025-04-14 11:08:32 +0100

    further changes

--- playwright-e2e/page-objects/pages/hearings/hearing-schedule.po.ts
+++ playwright-e2e/page-objects/pages/hearings/hearing-schedule.po.ts
@@ -130,7 +130,19 @@ export class HearingSchedulePage extends Base {
     if (await scheduleButton.isVisible()) {
       await scheduleButton.click();
       await this.goToSessionDetailsButton.click();
-      await expect(this.sessionBookingPage.heading).toBeVisible();
+
+      await expect
+        .poll(
+          async () => {
+            return await this.sessionBookingPage.heading.isVisible();
+          },
+          {
+            intervals: [2_000],
+            timeout: 10_000,
+          },
+        )
+        .toBeTruthy();
+      // await expect(this.sessionBookingPage.heading).toBeVisible();
 
       //delete session from inside of session details page, if available
       if (await this.deleteSessionInSessionDetailsButton.isVisible()) {
