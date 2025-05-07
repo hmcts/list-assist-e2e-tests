425c9d99a4aa5d42da4239c071b0005b7a0eee23
Rhys Heal <rhys.heal@justice.gov.uk>
2025-05-07 15:10:33 +0100

    separate setup blocks

--- playwright-e2e/global.setup.ts
+++ playwright-e2e/global.setup.ts
@@ -2,42 +2,44 @@ import { test as setup } from './fixtures';
 import { LoginPage } from './page-objects/pages/login.po';
 import { isSessionValid } from './utils';
 
-setup(
-  'Setup test user and create new case',
-  async ({ page, config, homePage, addNewCasePage, hearingSchedulePage, dataUtils }) => {
-    //test user setup
-    const user = config.users.testUser;
-    if (!isSessionValid(user.sessionFile, user.cookieName!)) {
-      await page.goto(config.urls.baseUrl);
-      await new LoginPage(page).login(config.users.testUser);
-    }
-
+// Setup block for test user login
+setup('Setup test user', async ({ page, config, hearingSchedulePage }) => {
+  // Test user setup
+  const user = config.users.testUser;
+  if (!isSessionValid(user.sessionFile, user.cookieName!)) {
     await page.goto(config.urls.baseUrl);
     await new LoginPage(page).login(config.users.testUser);
+  }
 
-    //empties cart if there is anything present
-    await hearingSchedulePage.sidebarComponent.emptyCaseCart();
+  await page.goto(config.urls.baseUrl);
+  await new LoginPage(page).login(config.users.testUser);
 
-    //creates new case for all tests
-    await homePage.sidebarComponent.openAddNewCasePage();
+  // Empties cart if there is anything present
+  await hearingSchedulePage.sidebarComponent.emptyCaseCart();
+});
 
-    // Generate case details
-    process.env.HMCTS_CASE_NUMBER = 'HMCTS_CN_' + dataUtils.generateRandomAlphabetical(10).toUpperCase();
-    process.env.CASE_NAME = 'AUTO_' + dataUtils.generateRandomAlphabetical(10).toUpperCase();
+// Setup block for creating a new case
+setup('Create new case', async ({ homePage, addNewCasePage, dataUtils }) => {
+  // Navigate to Add New Case page
+  await homePage.sidebarComponent.openAddNewCasePage();
 
-    const caseData = {
-      hmctsCaseNumberHeaderValue: addNewCasePage.CONSTANTS.HMCTS_CASE_NUMBER_HEADER_VALUE,
-      caseNameHeaderValue: addNewCasePage.CONSTANTS.CASE_NAME_HEADER_VALUE,
-      jurisdiction: addNewCasePage.CONSTANTS.JURISDICTION_FAMILY,
-      service: addNewCasePage.CONSTANTS.SERVICE_DIVORCE,
-      caseType: addNewCasePage.CONSTANTS.DECREE_ABSOLUTE_CASE_TYPE,
-      region: addNewCasePage.CONSTANTS.REGION_WALES,
-      cluster: addNewCasePage.CONSTANTS.CLUSTER_WALES_CIVIL_FAMILY_TRIBUNALS,
-      hearingCentre: addNewCasePage.CONSTANTS.HEARING_CENTRE_CARDIFF,
-      hearingTypeRef: addNewCasePage.CONSTANTS.HEARING_TYPE_APPLICATION_REF,
-      currentStatus: addNewCasePage.CONSTANTS.CURRENT_STATUS_AWAITING_LISTING,
-    };
+  // Generate case details
+  process.env.HMCTS_CASE_NUMBER = 'HMCTS_CN_' + dataUtils.generateRandomAlphabetical(10).toUpperCase();
+  process.env.CASE_NAME = 'AUTO_' + dataUtils.generateRandomAlphabetical(10).toUpperCase();
 
-    await addNewCasePage.addNewCaseWithMandatoryData(caseData, process.env.HMCTS_CASE_NUMBER, process.env.CASE_NAME);
-  },
-);
+  const caseData = {
+    hmctsCaseNumberHeaderValue: addNewCasePage.CONSTANTS.HMCTS_CASE_NUMBER_HEADER_VALUE,
+    caseNameHeaderValue: addNewCasePage.CONSTANTS.CASE_NAME_HEADER_VALUE,
+    jurisdiction: addNewCasePage.CONSTANTS.JURISDICTION_FAMILY,
+    service: addNewCasePage.CONSTANTS.SERVICE_DIVORCE,
+    caseType: addNewCasePage.CONSTANTS.DECREE_ABSOLUTE_CASE_TYPE,
+    region: addNewCasePage.CONSTANTS.REGION_WALES,
+    cluster: addNewCasePage.CONSTANTS.CLUSTER_WALES_CIVIL_FAMILY_TRIBUNALS,
+    hearingCentre: addNewCasePage.CONSTANTS.HEARING_CENTRE_CARDIFF,
+    hearingTypeRef: addNewCasePage.CONSTANTS.HEARING_TYPE_APPLICATION_REF,
+    currentStatus: addNewCasePage.CONSTANTS.CURRENT_STATUS_AWAITING_LISTING,
+  };
+
+  // Create the new case
+  await addNewCasePage.addNewCaseWithMandatoryData(caseData, process.env.HMCTS_CASE_NUMBER, process.env.CASE_NAME);
+});
