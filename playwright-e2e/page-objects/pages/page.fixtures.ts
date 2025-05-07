fc593c186826b6905f81477c156592be82239fa1
Rhys Heal <rhys.heal@justice.gov.uk>
2025-05-07 13:00:25 +0100

    updated formatting

--- playwright-e2e/page-objects/pages/page.fixtures.ts
+++ playwright-e2e/page-objects/pages/page.fixtures.ts
@@ -1,15 +1,15 @@
-import {CaseDetailsPage} from './cases/case-details.po';
-import {CaseSearchPage} from './cases/case-search.po';
-import {SessionBookingPage} from './hearings/session-booking.po.ts';
-import {HearingSchedulePage} from './hearings/hearing-schedule.po';
-import {HomePage} from './home.po';
-import {LoginPage} from './login.po';
-import {AddNewCasePage} from './cases/add-new-case.po.ts';
-import {EditNewCasePage} from './cases/edit-case.po.ts';
-import {NewParticipantPage} from './participants/new-participants.po.ts';
-import {EditParticipantPage} from './participants/edit-participants.po.ts';
-import {ViewReportsPage} from './reports/view-reports.po.ts';
-import {ListingRequirementsPage} from "./cases/listing-requirments.po.ts";
+import { CaseDetailsPage } from './cases/case-details.po';
+import { CaseSearchPage } from './cases/case-search.po';
+import { SessionBookingPage } from './hearings/session-booking.po.ts';
+import { HearingSchedulePage } from './hearings/hearing-schedule.po';
+import { HomePage } from './home.po';
+import { LoginPage } from './login.po';
+import { AddNewCasePage } from './cases/add-new-case.po.ts';
+import { EditNewCasePage } from './cases/edit-case.po.ts';
+import { NewParticipantPage } from './participants/new-participants.po.ts';
+import { EditParticipantPage } from './participants/edit-participants.po.ts';
+import { ViewReportsPage } from './reports/view-reports.po.ts';
+import { ListingRequirementsPage } from './cases/listing-requirments.po.ts';
 
 export interface PageFixtures {
   loginPage: LoginPage;
@@ -27,40 +27,40 @@ export interface PageFixtures {
 }
 
 export const pageFixtures = {
-  loginPage: async ({page}, use) => {
+  loginPage: async ({ page }, use) => {
     await use(new LoginPage(page));
   },
-  homePage: async ({page}, use) => {
+  homePage: async ({ page }, use) => {
     await use(new HomePage(page));
   },
-  hearingSchedulePage: async ({page}, use) => {
+  hearingSchedulePage: async ({ page }, use) => {
     await use(new HearingSchedulePage(page));
   },
-  caseSearchPage: async ({page}, use) => {
+  caseSearchPage: async ({ page }, use) => {
     await use(new CaseSearchPage(page));
   },
-  caseDetailsPage: async ({page}, use) => {
+  caseDetailsPage: async ({ page }, use) => {
     await use(new CaseDetailsPage(page));
   },
-  sessionBookingPage: async ({page}, use) => {
+  sessionBookingPage: async ({ page }, use) => {
     await use(new SessionBookingPage(page));
   },
-  addNewCasePage: async ({page}, use) => {
+  addNewCasePage: async ({ page }, use) => {
     await use(new AddNewCasePage(page));
   },
-  editNewCasePage: async ({page}, use) => {
+  editNewCasePage: async ({ page }, use) => {
     await use(new EditNewCasePage(page));
   },
-  newParticipantsPage: async ({page}, use) => {
+  newParticipantsPage: async ({ page }, use) => {
     await use(new NewParticipantPage(page));
   },
-  editParticipantsPage: async ({page}, use) => {
+  editParticipantsPage: async ({ page }, use) => {
     await use(new EditParticipantPage(page));
   },
-  viewReportsPage: async ({page}, use) => {
+  viewReportsPage: async ({ page }, use) => {
     await use(new ViewReportsPage(page));
   },
-  listingRequirementsPage: async ({page}, use) => {
+  listingRequirementsPage: async ({ page }, use) => {
     await use(new ListingRequirementsPage(page));
-  }
-}
+  },
+};
