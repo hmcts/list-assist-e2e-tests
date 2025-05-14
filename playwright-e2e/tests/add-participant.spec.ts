8d3d217b2eee76276368c348b8035cd9278f79a6
Rhys Heal <rhys.heal@justice.gov.uk>
2025-05-14 16:46:04 +0100

    replaced config

--- playwright-e2e/tests/add-participant.spec.ts
+++ playwright-e2e/tests/add-participant.spec.ts
@@ -6,6 +6,7 @@ test.use({
 });
 
 test.describe('Add participant @add-participant', () => {
+  test.describe.configure({ mode: 'serial' });
   test.beforeEach(async ({ page }) => {
     await page.goto(config.urls.baseUrl);
   });
