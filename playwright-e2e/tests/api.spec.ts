7c5fd4fbb0d62e39da2d046e8c220e297387bd57
Jay Ashton <jay.ashton@justice.gov.uk>
2025-05-15 15:39:22 +0100

    Tidy up

--- playwright-e2e/tests/api.spec.ts
+++ playwright-e2e/tests/api.spec.ts
@@ -1,7 +1,8 @@
 import { test } from "../fixtures.js";
 import { HmiUtils } from "../utils/hmi.utils.js";
 
-test("API @api", async ({ config }) => {
+// only used for testing
+test.skip("API @api", async ({ config }) => {
   const payload = config.data.hearingRequest;
   console.log(payload);
   const sessions = await HmiUtils.requestHearing(payload);
