c410c546f70698b9a922c97bb2b2b2e7a20b157d
Jay Ashton <jay.ashton@justice.gov.uk>
2025-05-20 11:33:23 +0100

    Use common pw library

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,38 +1,9 @@
-import playwright from "eslint-plugin-playwright";
+import { LintingConfig } from "@hmcts/playwright-common";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  tseslint.configs.recommended,
-  {
-    ignores: [
-      ".yarn/**",
-      "eslint.config.mjs",
-      ".pnp.cjs",
-      ".pnp.loader.mjs",
-      "format-v4-audit.cjs",
-    ],
-  },
-  {
-    files: ["**/*.ts"],
-    plugins: {
-      "@typescript-eslint": tseslint.plugin,
-    },
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        project: "./tsconfig.json",
-      },
-    },
-    rules: {
-      "@typescript-eslint/no-floating-promises": "error",
-    },
-  },
-  {
-    ...playwright.configs["flat/recommended"],
-    rules: {
-      ...playwright.configs["flat/recommended"].rules,
-      "playwright/expect-expect": "off",
-    },
-    files: ["playwright-e2e/**/*.ts"],
-  },
+  LintingConfig.tseslintRecommended,
+  LintingConfig.ignored,
+  LintingConfig.tseslintPlugin,
+  LintingConfig.playwright
 );
