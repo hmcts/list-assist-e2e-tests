3cfed7a71268eb56a63b4c5f7f26aae16cfb8936
Rhys Heal <rhys.heal@justice.gov.uk>
2025-06-05 15:43:37 +0100

    trying pipeline with session removed?

--- playwright-e2e/global.setup.ts
+++ playwright-e2e/global.setup.ts
@@ -1,10 +1,25 @@
 import { test as setup } from './fixtures';
 import { isSessionValid } from './utils';
+import fs from 'fs/promises';
+import path from 'path';
 
 setup.describe('Global Setup', () => {
   setup.beforeAll(async ({ dataUtils }) => {
     // Update bank holidays file if needed
     dataUtils.updateBankHolidaysFileIfNeeded();
+  });
+
+  setup('Remove session file', async () => {
+    const sessionsDir = path.resolve(path.dirname(new URL('', import.meta.url).pathname), './.sessions');
+    const automationTestSession = path.join(sessionsDir, 'automationtest.json');
+
+    try {
+      await fs.unlink(automationTestSession);
+    } catch (err) {
+      if ((err as NodeJS.ErrnoException).code !== 'ENOENT') {
+        console.error('Error removing automationtest.json:', err);
+      }
+    }
   });
 
   setup('Setup test user', async ({ loginPage, page, config }) => {
