d45780a8a23fcd25d70b105b24285b6700170035
Rhys Heal <rhys.heal@justice.gov.uk>
2025-11-10 11:18:04 +0000

    process.env.SKIP_CREATE_CASE = "true";

--- playwright-e2e/tests/bvt/amend-request-after-listing.spec.ts
+++ playwright-e2e/tests/bvt/amend-request-after-listing.spec.ts
@@ -9,6 +9,8 @@ import {
 } from "../../page-objects/pages/index.ts";
 import { SessionBookingPage } from "../../page-objects/pages/hearings/session-booking.po.ts";
 
+process.env.SKIP_CREATE_CASE = "true";
+
 test.describe("HMI Amend API tests after listing @amend-api-test-after-listing", () => {
   //using test.slow() because this test takes longer than 3 minutes to complete
   test.slow();
