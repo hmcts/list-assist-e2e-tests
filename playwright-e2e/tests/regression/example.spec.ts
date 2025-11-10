ad0b2d9e98245469d9a55e78d9c184373f065e2f
Rhys Heal <rhys.heal@justice.gov.uk>
2025-11-10 15:59:54 +0000

    update

---
+++ playwright-e2e/tests/regression/example.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from "../../fixtures.js";
+import { config } from "../../utils/index.js";
+
+process.env.SKIP_CREATE_CASE = "true";
+
+test.use({
+  storageState: config.users.testUser.sessionFile,
+});
+
+test.describe("Another example @another-example", () => {
+  console.log("This is another example test file.");
+});
