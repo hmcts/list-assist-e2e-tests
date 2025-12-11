090183df397138641a9ab0e660a88116127c77e4
Rhys Heal <rhys.heal@justice.gov.uk>
2025-12-11 09:44:12 +0000

    removed tag

--- playwright-e2e/tests/bvt/add-participant.spec.ts
+++ playwright-e2e/tests/bvt/add-participant.spec.ts
@@ -102,7 +102,7 @@ test.describe("Add participant @add-participant", () => {
     await homePage.waitForHomePageLoad();
   });
 
-  test("Case history should display correct event codes @codes", async ({
+  test("Case history should display correct event codes", async ({
     homePage,
     dataUtils,
     caseHistoryPage,
