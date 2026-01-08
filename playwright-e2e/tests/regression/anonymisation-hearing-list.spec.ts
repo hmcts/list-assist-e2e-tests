9cf0d0a27aebfebee2f751e36fe2769c91c87ce7
Rhys Heal <rhys.heal@justice.gov.uk>
2026-01-08 11:08:42 +0000

    updated further conditional variables

--- playwright-e2e/tests/regression/anonymisation-hearing-list.spec.ts
+++ playwright-e2e/tests/regression/anonymisation-hearing-list.spec.ts
@@ -449,6 +449,10 @@ test.describe("Hearing List anonymisation @anonymisation @regression", () => {
       sessionBookingPage.CONSTANTS.CASE_LISTING_SESSION_DURATION_1_00,
       sessionBookingPage.CONSTANTS.CASE_LISTING_SESSION_STATUS_TYPE_RELEASED,
       sessionBookingPage.CONSTANTS.AUTO_JUDICIAL_OFFICE_HOLDER_03,
+      "AB",
+      undefined,
+      `Automation internal comments ${process.env.HMCTS_CASE_NUMBER}`,
+      `Automation external comments ${process.env.HMCTS_CASE_NUMBER}`,
     );
 
     await expect(
