f92d3afd6f74beef049f307fa5963bfeb9d404a9
Rhys Heal <rhys.heal@justice.gov.uk>
2026-03-02 11:44:54 +0000

    added code to wait for elements to appear

--- playwright-e2e/page-objects/pages/hearings/session-booking.po.ts
+++ playwright-e2e/page-objects/pages/hearings/session-booking.po.ts
@@ -249,6 +249,7 @@ export class SessionBookingPage extends Base {
 
       // Click the desired option by visible text
       const johOption = dropdownMenu.getByText(johName, { exact: true });
+      await johOption.waitFor({ state: "visible", timeout: 5000 });
       await johOption.click();
     }
 
