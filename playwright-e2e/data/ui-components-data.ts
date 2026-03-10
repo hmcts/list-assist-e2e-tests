a82ae6c6d6eaf86f7bdfbe7127daae8baaef675b
Rhys Heal <rhys.heal@justice.gov.uk>
2026-03-10 14:01:23 +0000

    added check of all sidebar menus for smoke test

---
+++ playwright-e2e/data/ui-components-data.ts
@@ -0,0 +1,18 @@
+export const sidebarMenu = {
+  menuNames: [
+    "Home",
+    "Work History",
+    "Search",
+    "Participants",
+    "Cases ",
+    "Hearings",
+    "Reports",
+    "Online Reports",
+    "InHearing",
+    "Administration",
+    "Multi Day Cart",
+    "Listing Cart",
+    "Case Cart",
+    "Quick Listing",
+  ],
+};
